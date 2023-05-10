54b19e6e4c7afd3cf356cdde619edba9a009577d
Emil Kjelsrud <emil.kjelsrud@intility.no>
2023-05-10 11:55:10 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Kon'nitchiwa, %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Ni hao, %s!", r.URL.Path[1:])
 }
